1e759e9f1176cf1ab38a7e564f94626a67d7edf8
gab:O <202110208@gordoncollege.edu.ph>
2025-12-21 21:32:58 +0800

    Add minimal open-next.config.ts

--- open-next.config.js
+++
@@ -1,7 +0,0 @@
-// OpenNext configuration for Cloudflare
-// Simple config without imports - OpenNext will handle the rest
-
-/** @type {import('@opennextjs/cloudflare').OpenNextConfig} */
-const config = {};
-
-module.exports = config;
