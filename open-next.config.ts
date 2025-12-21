3ef745f9121ce97117575678ce81e6462eab4f1e
gab:O <202110208@gordoncollege.edu.ph>
2025-12-21 21:18:36 +0800

    Switch to OpenNext adapter for Cloudflare

---
+++ open-next.config.ts
@@ -0,0 +1,6 @@
+// @ts-nocheck
+// OpenNext config for Cloudflare Pages
+// Package is installed during Cloudflare build, not locally
+import { defineCloudflareConfig } from '@opennextjs/cloudflare';
+
+export default defineCloudflareConfig({});
