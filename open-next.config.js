ab81dc92968a2844f7a9cab1251d84fb64ca7c83
gab:O <202110208@gordoncollege.edu.ph>
2025-12-21 21:26:37 +0800

    Add OpenNext config (JS format)

---
+++ open-next.config.js
@@ -0,0 +1,7 @@
+// OpenNext configuration for Cloudflare
+// Simple config without imports - OpenNext will handle the rest
+
+/** @type {import('@opennextjs/cloudflare').OpenNextConfig} */
+const config = {};
+
+module.exports = config;
