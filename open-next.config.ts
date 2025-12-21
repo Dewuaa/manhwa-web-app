9709d98e12482bc9afabe68a52e327136736855d
gab:O <202110208@gordoncollege.edu.ph>
2025-12-21 21:22:37 +0800

    Remove OpenNext config - use defaults

--- open-next.config.ts
+++
@@ -1,6 +0,0 @@
-// @ts-nocheck
-// OpenNext config for Cloudflare Pages
-// Package is installed during Cloudflare build, not locally
-import { defineCloudflareConfig } from '@opennextjs/cloudflare';
-
-export default defineCloudflareConfig({});
